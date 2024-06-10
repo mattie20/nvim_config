ee1c2acd9dc2724eeb7c59af51358c8245b1dd2a
mattie <mattie@Debian.Debian>
2024-06-09 20:29:05 -0400

    Removed anything superfluous. Replaced telescope with fzf-lua for less lag

--- init.lua
+++ init.lua
@@ -1,65 +1,33 @@
 --=============== General Settings ============== 
 
-vim.opt.syntax = "on"
-
-vim.opt.compatible = false
-
 vim.opt.showmatch = true
 
 vim.opt.incsearch = true
 
-vim.opt.shiftwidth = 4
-
-vim.opt.autoindent = true
-
 -- Install wl-clipboard to use clipboard in wayland
 vim.opt.clipboard = "unnamedplus"
 
-vim.opt.mouse = 'a'
-
-vim.opt.ttyfast = true
-
-vim.opt.spell = false
-
-vim.opt.filetype = 'on'
-
-vim.opt.filetype.plugin = 'on'
-
-vim.opt.filetype.indent = 'on'
-
 vim.opt.number = true
-
-vim.opt.smartindent = true
 
 vim.opt.swapfile = false
 
-vim.opt.backup = false
-
-vim.undodir = os.getenv("HOME") .. "/.nvim/undodir"
-
 vim.opt.undofile = true
 
-vim.opt.updatetime = 50
-
---"set cursorline" set tabstop=4 set scrolloff=4
+-- vim.opt.updatetime = 50
 
 vim.opt.ignorecase = true
 
-vim.opt.hlsearch = false
-
 vim.opt.termguicolors = true
+
+vim.opt.shiftwidth = 4
+
+vim.opt.timeoutlen = 500
 
 --================= Key Bindings ===============
 
-vim.keymap.set({ 'i', 'v'}, 'jk', '<esc>')
+vim.keymap.set({'i','t'}, 'jk', '<C-\\><C-n>')
 
-vim.keymap.set({ 'i', 'v'}, 'kj', '<esc>')
-
-vim.keymap.set({'n','v'}, '<S-j>', 'gT')
-
-vim.keymap.set({'n','v'}, '<S-k>', 'gt')
-
-vim.keymap.set('n', '<S-e>', '5<C-e>')
+vim.keymap.set({'n','t'}, '<tab>', 'gt')
 
 vim.keymap.set({'n','v'}, ';', '$')
 
@@ -71,86 +39,73 @@ vim.keymap.set('n', '<A-k>', 'ddkP')
 
 vim.keymap.set('n', '<A-j>', 'ddjP')
 
-vim.keymap.set( 'n', '<space>ff', '<cmd>lua require("telescope.builtin").find_files({cwd = "$HOME", hidden = true})<CR>', {silent = true})
+vim.keymap.set('n', '<space>ff', '<cmd>lua require("fzf-lua").files()<CR>', { silent = true })
 
--- vim.keymap.set( 'n', '<space>ff', require("telescope.builtin").find_files({cwd = "/", hidden = true}), {silent = true})
+vim.keymap.set('n', '<space>fg', '<cmd>lua require("fzf-lua").live_grep()<CR>', { silent = true })
 
-vim.keymap.set( 'n', '<space>fg', '<cmd>lua require("telescope.builtin").live_grep({cwd = "$HOME" , hidden = true})<CR>', {silent = true})
+vim.keymap.set('n', '<space>fl', '<cmd>lua require("fzf-lua").lines()<CR>', { silent = true })
 
-vim.keymap.set( 'n', '<space>fs', '<cmd>lua require("telescope.builtin").grep_string({grep_open_files = true, hidden = true})<CR>', {silent = true})
-
-vim.keymap.set( 'n', '<space>fb', '<cmd>lua require("telescope.builtin").current_buffer_fuzzy_find({ previewer = false })<CR>', { noremap=true, silent = true})
-
---==================== Plug ins =================
+--==================== Plugins =================
 
 local Plug = vim.fn['plug#']
 
 vim.fn['plug#begin']('~/.config/nvim/plugged')
 
---Plug 'honza/vim-snippets'
---Plug 'mhinz/vim-startify'
-
 --============= Themes =========================
 Plug 'tanvirtin/monokai.nvim'
 Plug 'sainnhe/sonokai'
-Plug('catppuccin/nvim', {as = 'catppuccin '})
 
 --============= Plugins =========================
-Plug 'nvim-treesitter/nvim-treesitter'
-Plug 'nvim-lua/plenary.nvim'
-Plug('nvim-telescope/telescope.nvim', {branch = '0.1.x'})
+Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
+Plug('ibhagwan/fzf-lua', { ['branch'] = 'main'})
 
 vim.fn['plug#end']()
--- #66d9ef #76cce0
-vim.api.nvim_set_hl(0,'statline', { fg = '#66d9ef', bg = '#3b3e48'})
-vim.cmd[[set statusline=\ [%{mode()}\]\ %#statline#%<%F\%m\%#statusline#%=\ \[\ %P\ Line:%l\/%L\ ]\ ]]
+
 vim.cmd[[ colorscheme sonokai ]]
 
+--============= Statusline ======================
+vim.api.nvim_set_hl(0,'statline', { fg = '#66d9ef', bg = '#3b3e48'})
+vim.cmd[[set statusline=\ [%{mode()}\]\ \ %#statline#%<%f\%m\%#statusline#%=\ \[\ %P\ Line:%l\/%L\ ]\ ]]
 
-vim.g.vimwiki_list = {{syntax = 'markdown'}}
 
 require'nvim-treesitter.configs'.setup{
 
     ensure_installed = {"c", "cpp", "lua", "python", "bash", "markdown"},
 
-    sync_install = false,
-
     auto_install = true,
     
     highlight = {
 	enable = true,
-
-	disable = {"markdown"},
-	additional_vim_regex_highlighting = false
     },
 
     incremental_selection = {
 	enable = true,
         keymaps = {
-          init_selection = '<CR>',
-          scope_incremental = '<CR>',
+          -- init_selection = '<CR>',
+          -- scope_incremental = '<CR>',
           node_incremental = '<TAB>',
-          node_decremental = '<S-TAB>',
+          node_decremental = '<S-TAB>'
         },
     },
     
     indentexpr = {
 	enable = true
-    },
-}
-
-
-require('telescope').setup{
-
-    defaults={
-	layout_strategy = "flex",
-	preview_cuttoff = 1
     }
 }
 
-
-
-
+require'fzf-lua'.setup {
+    
+    winopts = {
+	preview = {
+    	    layout = 'vertical',
+	    vertical = 'up:45%'
+    	}
+    },
+    
+    fzf_opts = {
+        ["--scheme"]  = "path"
+    }
+}
 
 
 
